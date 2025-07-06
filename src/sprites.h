19b28d5d050c3306ec43947ef09fa38ddf2d180b
EmelyFuentes <00126025@uca.edu.sv>
2025-07-06 14:46:52 -0600

    Añadir funcion de los sprites

---
+++ src/sprites.h
@@ -0,0 +1,9 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+#include <string>
+
+// Función para mostrar gráficos ASCII del juego (original sin modificaciones)
+std::string sprite(std::string typeSprite);
+
+#endif
