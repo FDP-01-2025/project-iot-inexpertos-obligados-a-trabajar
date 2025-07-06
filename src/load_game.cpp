5e09b86130f3fcae820e87721015bd3899e3f242
Rodrigo Alejandro Campos Cierra <00167625@uca.edu.sv>
2025-07-06 14:57:41 -0600

    Lectura de dificultad y configuración de jugadores desde el archivo

--- src/load_game.cpp
+++ src/load_game.cpp
@@ -22,4 +22,44 @@ void load_game(std::vector<std::vector<int>>& bombXY) {
         players[i].is_alive = false;
         players[i].points = 0;
     }
+    while (std::getline(file, line)) {
+        if (line.empty()) continue;
+
+        if (line.find("max_players=") == 0) {
+            game_data.max_players = std::stoi(line.substr(12));
+        }
+        else if (line.find("difficulty_rows=") == 0) {
+            difficulty.max_rows = std::stoi(line.substr(16));
+        }
+        else if (line.find("difficulty_columns=") == 0) {
+            difficulty.max_columns = std::stoi(line.substr(19));
+        }
+        else if (line.find("difficulty_bombs=") == 0) {
+            difficulty.max_bombs = std::stoi(line.substr(17));
+        }
+        else if (line.find("difficulty_bullets=") == 0) {
+            difficulty.max_bullets = std::stoi(line.substr(19));
+        }
+        else if (line.find("difficulty_shields=") == 0) {
+            difficulty.max_shields = std::stoi(line.substr(19));
+        }
+        else if (line.find("player") == 0 && line.find("_name=") != std::string::npos) {
+            size_t p1 = line.find("player") + 6;
+            size_t p2 = line.find("_name=");
+            int idx = std::stoi(line.substr(p1, p2 - p1));
+            std::string name = line.substr(p2 + 6);
+            if (idx >=0 && idx < 4) {
+                players[idx].name = name;
+                players[idx].is_alive = true;
+            }
+        }
+        else if (line.find("player") == 0 && line.find("_points=") != std::string::npos) {
+            size_t p1 = line.find("player") + 6;
+            size_t p2 = line.find("_points=");
+            int idx = std::stoi(line.substr(p1, p2 - p1));
+            int pts = std::stoi(line.substr(p2 + 8));
+            if (idx >= 0 && idx < 4) {
+                players[idx].points = pts;
+            }
+        }
 }
