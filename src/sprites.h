811a63e3de79318a1b1b482eb7de3af9879fc93d
Oscar Ignacio Gil Villalta <00186825@uca.edu.sv>
2025-07-07 14:31:40 -0600

    se mejoro errores visuales con el tutorial

--- src/sprites.h
+++ src/sprites.h
@@ -172,22 +172,22 @@ R"(        ```--. . , ; .--'''
        "\033[0m";
  std::string tutorial =
     "\033[96m ________________________________________________________________________________________________________________________________________ \033[0m\n"
-    "\033[96m|\033[0m                                                                                                                                        \033[96m|\033[0m\n"
-    "\033[96m|\033[0m                                                                TUTORIAL DE JUEGO                                                       \033[96m|\033[0m\n"
-    "\033[96m|----------------------------------------------------------------------------------------------------------------------------------------|\033[0m\n"
-    "\033[96m|\033[0m  1. Selecciona el modo en el que deseas jugar(solitario o multijugador).                                                               \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  2. Selecciona la dificultad que deseas jugar(Facil, Normal,  Dificil).                                                                \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  3. Dependiendo la dificultad escogida se otorgará al jugador una cantidad de escudos y balas para la pistola.                         \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  4. Ingresa tu nombre de usuario(Sin espacios).                                                                                         \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  5. Ingresa el numero de fila que desea seleccionar(que esté dentro de los parámetros).                                                \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  6. Ingrese el numero de columna que desea seleccionar(que esté dentro de los parámetros).                                             \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  7. Se le darán 4 opciones (1=excavar, 2=protegerse, 3=disparar con pistola, 4=Guardar y salir).                                       \033[96m|\033[0m\n"
-    "\033[96m|\033[0m    7.1 \"1=excavar\": Le permite abrir una celda que no se ha marcado aún con la probabilidad que salga un tesoro o una bomba.            \033[96m|\033[0m\n"
-    "\033[96m|\033[0m    7.2 \"2=protegerse\": Usa uno de los escudos asignados para evitar perder si pisa una bomba (se pierde lo use o no).                   \033[96m|\033[0m\n"
-    "\033[96m|\033[0m    7.3 \"3=disparar con pistola\": Usa una bala asignada para ganar puntos extra si acierta a una bomba (si falla, pierde la bala).      \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  8. \"4=Guardar y salir\": Deja la partida hasta donde se ha llegado, con todos los datos guardados, y se retira del juego.              \033[96m|\033[0m\n"
-    "\033[96m|\033[0m  9. El objetivo del juego es evitar todas las bombas en el tablero y conseguir la mayor cantidad de puntos.                          \033[96m|\033[0m\n"
-    "\033[96m|________________________________________________________________________________________________________________________________________|\033[0m\n";
+    "\033[96m|\033[0m                                                                                                                                       \033[96m|\033[0m\n"
+    "\033[96m|\033[0m                                                                TUTORIAL DE JUEGO                                                      \033[96m|\033[0m\n"
+    "\033[96m|---------------------------------------------------------------------------------------------------------------------------------------|\033[0m\n"
+    "\033[96m|\033[0m  1. Selecciona el modo en el que deseas jugar(solitario o multijugador).                                                              \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  2. Selecciona la dificultad que deseas jugar(Facil, Normal,  Dificil).                                                               \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  3. Dependiendo la dificultad escogida se otorgará al jugador una cantidad de escudos y balas para la pistola.                       \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  4. Ingresa tu nombre de usuario(Sin espacios).                                                                                       \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  5. Ingresa el numero de fila que desea seleccionar(que esté dentro de los parámetros).                                             \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  6. Ingrese el numero de columna que desea seleccionar(que esté dentro de los parámetros).                                          \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  7. Se le darán 4 opciones (1=excavar, 2=protegerse, 3=disparar con pistola, 4=Guardar y salir).                                     \033[96m|\033[0m\n"
+    "\033[96m|\033[0m   7.1 \"1=excavar\": Le permite abrir una celda que no se ha marcado aún con la probabilidad que salga un tesoro o una bomba.          \033[96m|\033[0m\n"
+    "\033[96m|\033[0m   7.2 \"2=protegerse\": Usa uno de los escudos asignados para evitar perder si pisa una bomba (se pierde lo use o no).                  \033[96m|\033[0m\n"
+    "\033[96m|\033[0m   7.3 \"3=disparar con pistola\": Usa una bala asignada para ganar puntos extra si acierta a una bomba (si falla, pierde la bala).      \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  8. \"4=Guardar y salir\": Deja la partida hasta donde se ha llegado, con todos los datos guardados, y se retira del juego.             \033[96m|\033[0m\n"
+    "\033[96m|\033[0m  9. El objetivo del juego es evitar todas las bombas en el tablero y conseguir la mayor cantidad de puntos.                           \033[96m|\033[0m\n"
+    "\033[96m|_______________________________________________________________________________________________________________________________________|\033[0m\n";
 
 };
 
